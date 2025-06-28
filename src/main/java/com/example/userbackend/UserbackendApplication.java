d6f6c1579f20264c17f6823f30e2e2f981893c26
pranjal-katkar <pranjalkatkar70@gmail.com>
2025-06-28 21:37:25 +0530

    Healthcheck added

--- src/main/java/com/example/userbackend/UserbackendApplication.java
+++ src/main/java/com/example/userbackend/UserbackendApplication.java
@@ -3,26 +3,25 @@ package com.example.userbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @ComponentScan("com.example.userbackend")
 @RestController
-
-
 public class UserbackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserbackendApplication.class, args);
-	}	
-	@GetMapping("/")
-	public String getMethodName() {
-		return new String("hELLO ");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserbackendApplication.class, args);
+    }
 
+    @GetMapping("/")
+    public String home() {
+        return "User Management Backend is Running 🚀";
+    }
 
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK";
+    }
 }
